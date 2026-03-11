7f84ce396c7d86f02deb3c3871c7f1031211c46c
jlnshly <jelaineashleyramos@gmail.com>
2026-03-12 01:25:43 +0800

    code working after fixing errors

---
+++ print_the_quotient.py
@@ -0,0 +1,5 @@
+#Assign variable and input
+first_number = int(input("Enter first number: "))
+second_number = int(input("Enter second number: "))
+#Code the operation
+print("Quotient: ", first_number // second_number)
